acedc25f36f871d4ab49fa06d27ab1888b919568
xiong1028 <lixiong1124@gmail.com>
2019-02-08 15:20:51 -0500

    modify the db.class.php

--- db/db.class.php
+++ db/db.class.php
@@ -12,12 +12,12 @@ class DB{
         'DB_HOST'=>'localhost',
         'DB_USER'=>'lamp2user',
         'DB_PASSWD' =>'Test123!',
-        'DB_NAME'=>''
+        'DB_NAME'=>'microwave_path_data'
     );
     
 
-    function __constructor($hostname,$username,$pwd,$dbname){
-        $this->db_conn = new mysqli($hostname,$username,$pwd,$dbname);
+    function __constructor(){
+        $this->db_conn = new mysqli($this->config['DB_HOST'],$this->config['DB_USER'],$this->config['DB_PASSWD'],$this->config['DB_NAME']);
         if($this->db_conn->connect_errno){
             die("Could not connect to database server" . $hostname . "\n Error: " . $this->db_conn->connect_errno . "\n Report: " . $this->db_conn->connect_error . "\n");
         }
