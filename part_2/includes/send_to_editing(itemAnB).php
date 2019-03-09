fa3ab15680742b47fd486b45cbc8cd5fe28e08ed
DeboraCamargo <me.imagine.debby@gmail.com>
2019-03-09 13:20:37 -0500

    where to code

--- part_2/includes/send_to_editing(itemAnB).php
+++ part_2/includes/send_to_editing(itemAnB).php
@@ -50,21 +50,22 @@ $arrlength=count($paths);
 <div id="myModal_GeneralPath" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      ...
+      CODE TO EDIT GENERAL PATH GOES HERE
+
     </div>
   </div>
 </div>
-
 
 <div id="myModal_MidPoint" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      ...
+      CODE TO EDIT MIDPOINT( PART E) GOES HERE
+
     </div>
   </div>
 </div>
 
-
+<!--ITEM D - EDITING ENDPOINT -->
 <div id="myModal_EndPoint" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
